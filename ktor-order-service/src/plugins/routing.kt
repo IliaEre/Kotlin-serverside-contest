5fc1305de3df2532dbe0dcf4773de608d6bcbdc9
ilya <ilya.selfrealization@gmail.com>
2022-02-11 16:45:34 +0300

    small routing refactoring

--- ktor-order-service/src/plugins/routing.kt
+++ ktor-order-service/src/plugins/routing.kt
@@ -14,12 +14,17 @@ fun Application.routes(db: CoroutineDatabase) {
 
     routing {
         route("/v1/order") {
+            
             get("/{name}") {
-                call.parameters["name"]?.let { name ->
-                        db.getCollection<Order>().findOne(Order::uuid eq name)
-                            ?.also { call.respond(SuccessResponse(HttpStatusCode.OK, it)) }
-                            ?: call.respond(UnSuccessResponse<Any>(HttpStatusCode.NotFound))
-                    } ?: call.respond(UnSuccessResponse(HttpStatusCode.BadRequest, "not found name field"))
+                val name = call.parameters["name"]
+                if (name.isNullOrBlank()) {
+                    call.respond(UnSuccessResponse(HttpStatusCode.BadRequest, "not found name field"))
+                } else {
+                    db.getCollection<Order>().findOne(Order::uuid eq name)
+                        ?.let { call.respond(SuccessResponse(HttpStatusCode.OK, it)) }
+                        ?: call.respond(UnSuccessResponse<Any>(HttpStatusCode.NotFound))
+
+                }
             }
 
             post {
